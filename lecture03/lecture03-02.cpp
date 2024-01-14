f710a4ad2af79a11120642b58a6838015731168d
n-kev1n <kevin_@tuta.io>
2024-01-14 20:10:01 +0100

    converted Arr class to a generic class
    now it can accept different data types

--- lecture03/lecture03-02.cpp
+++ lecture03/lecture03-02.cpp
@@ -5,22 +5,52 @@ void pp(int& i)
     std::cout << &i << " " << i << " " << sizeof(i) << std::endl;
 }
 
-class IntArr {
+template <typename T>
+class DynamicArr {
     size_t m_size;
-    int* m_arr;
+    T* m_arr;
 
 public:
-    IntArr(size_t size)
+    DynamicArr(size_t size)
         : m_size(size)
-        , m_arr(new int[size])
+        , m_arr(new T[size])
     {
+        std::cout << "Array Contructor\n";
+    }
 
-        std::cout << "Array Contructor";
+    ~DynamicArr()
+    {
+        delete[] m_arr;
+        std::cout << "Array Destructor\n";
+    }
+
+    T get(size_t index) const
+    {
+        return m_arr[index];
+    }
+
+    void set(size_t index, T val)
+    {
+        m_arr[index] = val;
+    }
+
+    void print() const
+    {
+        for (size_t i = 0; i < m_size; i++) {
+            std::cout << i << " " << m_arr[i] << std::endl;
+        }
     }
 };
 
 int main(int argc, char* argv[])
 {
+
+    DynamicArr<int> myArr(10);
+
+    myArr.set(4, 30);
+    myArr.set(0, 90);
+
+    myArr.print();
 
     return 0;
 }
